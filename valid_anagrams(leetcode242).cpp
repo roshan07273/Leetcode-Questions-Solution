598fe28cdbc676bb854a009386e16d39d6e5ecc8
Roshan Raturi <77711686+roshan07273@users.noreply.github.com>
2023-03-23 16:30:01 +0530

    Create valid_anagrams(leetcode242).cpp

---
+++ valid_anagrams(leetcode242).cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        int freqt[26] = {0} , freqs[26] = {0};
+        for(char ch : s) freqs[ch - 'a']++;
+        for(char ch : t) freqt[ch - 'a']++;
+        for(int i = 0 ; i < 26 ; i++)
+        if(freqs[i] != freqt[i])return false ;
+         return true;
+    }
+    
+};
