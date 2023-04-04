ad2b84eb4de4c6833e22c6cd1098b7f82b3bbcbd
Roshan Raturi <77711686+roshan07273@users.noreply.github.com>
2023-04-05 05:12:55 +0530

    Create Leetcode 42(HArd problem).cpp

---
+++ Leetcode 42(HArd problem).cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int left = 0;
+        int right = n-1;
+        int leftmax = 0;
+        int rightmax = 0;
+        int result = 0;
+        while(left <= right){
+            if(height[left] <= height[right]){
+                if(height[left] >= leftmax) leftmax = height[left];
+                else result += leftmax-height[left];
+                left++;
+            }else{
+                if(height[right] >= rightmax) rightmax = height[right];
+                else result += rightmax-height[right];
+                right--;
+            }
+        }
+        return result;
+    }
+};
