6e3d0492c3c0886114af6c719cfad1b1d8e3b889
Roshan Raturi <77711686+roshan07273@users.noreply.github.com>
2023-04-05 20:47:31 +0530

    Create combination_sum.cpp

---
+++ combination_sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    
+public: 
+    void findCombinationSum(int index,int target ,vector<int>&arr ,vector<vector<int>>&ans ,vector<int>ds){
+        
+        if(index == arr.size()){
+            if(target == 0){
+                ans.push_back(ds);
+            }return;
+        }
+        if(arr[index] <= target){
+            ds.push_back(arr[index]);
+            findCombinationSum(index , target-arr[index] , arr , ans , ds);
+            ds.pop_back();
+        }
+        findCombinationSum(index+1 , target , arr , ans , ds);
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        
+        vector<vector<int>>ans;
+        vector<int>ds;
+        findCombinationSum(0 , target , candidates , ans , ds);
+        return ans;
+        
+    }
+};
