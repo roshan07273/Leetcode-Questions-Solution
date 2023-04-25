9685613e8a148a0129b42447d33b5f3e1ea92ac2
Roshan Raturi <77711686+roshan07273@users.noreply.github.com>
2023-04-25 15:26:07 +0530

    Create Minimum_path.cpp

---
+++ Minimum_path.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    int funct(int i , int j , vector<vector<int>>&grid , vector<vector<int>>&dp){
+        if(i == 0 && j == 0) return grid[i][j];
+        if( i < 0 || j < 0 ) return 1e9;
+       
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int up = grid[i][j] + funct(i-1 , j ,grid , dp);
+        int left = grid[i][j] + funct(i , j-1 , grid , dp);
+        
+        return dp[i][j] = min(left, up);
+    }
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>dp(n , vector<int>(m , -1));
+        return funct(n-1 , m-1 , grid , dp);
+    }
+};
