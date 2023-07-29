1097d3d82a4709c65850062b31e8501260afa8e7
Roshan Raturi <77711686+roshan07273@users.noreply.github.com>
2023-07-29 14:49:53 +0530

    Create key_pair.cpp

---
+++ key_pair.cpp
@@ -0,0 +1,26 @@
+//User function template for C++
+class Solution{
+public:	
+	// Function to check if array has 2 elements
+	// whose sum is equal to the given value
+	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	    // code here
+	    unordered_map<int,int>umap;
+	    for(int i=0;i<n;i++)
+	    umap[arr[i]]++;
+	    for(auto itr = umap.begin(); itr!=umap.end(); itr++)
+	    {
+	        int key = itr->first;
+	        int val = itr->second;
+	        int pair= x-key;
+	        if(pair == key){
+	            if(val > 1)
+	            return true;
+	        }else{
+	            if(umap.find(pair) != umap.end())
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+};
