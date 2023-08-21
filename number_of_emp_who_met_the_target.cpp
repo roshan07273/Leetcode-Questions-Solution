1cb8acf73f447fabeb234b72e39fe583c13fdaea
Roshan Raturi <77711686+roshan07273@users.noreply.github.com>
2023-08-21 21:07:42 +0530

    Create number_of_emp_who_met_the_target.cpp

---
+++ number_of_emp_who_met_the_target.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int n = hours.size();
+        int count = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(hours[i] >= target) count++;
+        }
+        return count;
+    }
+};
